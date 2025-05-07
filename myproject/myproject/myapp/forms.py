5d65f0062bacd0dea9248b16fcdd5e115c13de95
pannaton-dum <pannaton.dum@dome.tu.ac.th>
2025-05-07 16:39:44 +0700

    update code

--- myproject/myproject/myapp/forms.py
+++
@@ -1,8 +0,0 @@
-# /myproject/myapp/forms.py
-from django import forms
-from .models import Rental
-
-class RentalForm(forms.ModelForm):
-    class Meta:
-        model = Rental
-        fields = []  # No fields needed in the form; user and bike are set programmatically
