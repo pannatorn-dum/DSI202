ab61fbf3ef7b75c2f93472e451b1f6d8dcefeed2
pannaton-dum <pannaton.dum@dome.tu.ac.th>
2025-05-14 21:16:38 +0700

    update loginn

--- shop/urls.py
+++ shop/urls.py
@@ -11,14 +11,11 @@ urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('products/', views.product_list, name='product_list'),
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
-        # URL สำหรับล็อกอิน
-    path('login/', auth_views.LoginView.as_view(), name='account_login'),
-    # URL สำหรับออกจากระบบ
-    path('logout/', auth_views.LogoutView.as_view(), name='account_logout'),
-    # URL สำหรับหน้าแรก
-    path('', views.homepage, name='homepage'),
+    path('admin/', admin.site.urls),
+    path('', include('shopapp.urls')),  # your app
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
-
 # ✅ ให้ Django เสิร์ฟ media files (เช่น รูปภาพ)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
